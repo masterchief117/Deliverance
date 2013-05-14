b0809deaa14201d08f00924c95bd75f60af0eb13
masterchief117 <bstewart@catalystitservices.com>
2013-05-14 12:07:49 -0700

    Added field, removed constructor!

--- src/com/proof/of/concept/Dao.java
+++ src/com/proof/of/concept/Dao.java
@@ -4,9 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
 import oracle.jdbc.driver.OracleDriver;
-
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
 import com.mongodb.gridfs.GridFS;
@@ -20,24 +18,21 @@ import com.mongodb.gridfs.GridFSInputFile;
  */
 public abstract class Dao {
 
+	// Used for connection to a SQL db
+	// make sure to close if you open up a connection
 	private Connection connection;
+	// Used for creating statements to query for from the db
+	// might want to switch to prepareStatement to prevent SQL injection
+	// TODO
 	private Statement statement;
+	// results from a Query are stored in a ResultSet
 	private ResultSet result;
 
+	// Creates a conne
 	private MongoClient mongoClient;
 	private DB mongoDb;
 	private GridFS gridFs;
 	private GridFSInputFile gridFsInputFile;
-
-	/**
-	 * Set the driver up!
-	 * 
-	 * @param driverType
-	 *            driver to use EXAMPLE (new OracleDriver())
-	 */
-	public Dao(Object driverType) {
-		setDriver(driverType);
-	}
 
 	/**
 	 * Set the driver and connection up!
@@ -58,10 +53,23 @@ public abstract class Dao {
 	}
 
 	/**
+	 * Any time mongoClient is opened. CLOSE BEFORE leaving Dao layer!
+	 */
+	public void closeMongo() {
+		try {
+			if (mongoClient != null) {
+				mongoClient.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
 	 * Call close before exiting out of the Dao layer! Do not leave Connection
 	 * open!
 	 */
-	public void close() {
+	public void closeSQL() {
 		try {
 			if (result != null) {
 				result.close();
@@ -77,6 +85,11 @@ public abstract class Dao {
 		}
 	}
 
+	/**
+	 * Set the driver to DriverManager
+	 * 
+	 * @param o
+	 */
 	private void setDriver(Object o) {
 		if (o instanceof OracleDriver) {
 			try {
@@ -87,6 +100,16 @@ public abstract class Dao {
 		}
 	}
 
+	/**
+	 * Set the connection string to DriverManager, to push to this.connection
+	 * 
+	 * @param connection
+	 *            url
+	 * @param username
+	 *            un
+	 * @param password
+	 *            pw
+	 */
 	private void setConnection(String connection, String username,
 			String password) {
 		try {
